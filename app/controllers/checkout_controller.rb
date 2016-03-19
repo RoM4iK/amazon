46c32098c8d329d9f19baa3e55f2dc1fa9f1725c
Roman <w2@live.ru>
2016-03-19 15:43:38 +0200

    added validation to checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -4,6 +4,8 @@ class CheckoutController < ApplicationController
   before_action :authenticate_customer!
   before_action :get_current_order
 
+  around_action :handle_validation_errors
+
   steps :billing, :shipping, :delivery, :payment, :confirmation
 
   def show
@@ -48,7 +50,7 @@ class CheckoutController < ApplicationController
   def update_billing
     @billing_address = @order.billing_address || Address.new
     @billing_address.attributes = params.require(:address).permit(:address, :zipcode, :city, :phone, :country_id)
-    @billing_address.save
+    @billing_address.save!
     @order.billing_address = @billing_address
     @order.save
     redirect_to next_wizard_path
@@ -68,21 +70,26 @@ class CheckoutController < ApplicationController
       @shipping_address.attributes = params.require(:address).permit(
         :address, :zipcode, :city, :phone, :country_id
       )
-      @shipping_address.save
+      @shipping_address.save!
       @order.shipping_address = @shipping_address
-      @order.save!
+      @order.save
     end
     redirect_to next_wizard_path
   end
 
   def skip_shipping
     @order.shipping_address = @order.billing_address
-    @order.save!
+    @order.save
   end
 
   def update_delivery
-    @order.update params.require(:order).permit(:delivery_id)
-    redirect_to next_wizard_path
+    begin
+      @order.update params.require(:order).permit(:delivery_id)
+      redirect_to next_wizard_path
+    rescue ActionController::ParameterMissing
+      flash.now[:alert] = "You must select delivery method"
+      render_wizard
+    end
   end
 
   def payment
@@ -94,7 +101,7 @@ class CheckoutController < ApplicationController
     @credit_card.attributes = params.require(:credit_card).permit(
       :number, :cvv, :expiration_month, :expiration_year, :first_name, :last_name
     )
-    @credit_card.save
+    @credit_card.save!
     @order.credit_card = @credit_card
     @order.save
     redirect_to next_wizard_path
@@ -119,5 +126,14 @@ class CheckoutController < ApplicationController
   def redirect_with_message(step, message)
     flash[:alert] = message
     redirect_to wizard_path(step)
+  end
+
+  def handle_validation_errors
+    begin
+      yield
+    rescue ActiveRecord::RecordInvalid => e
+      flash.now[:alert] = e.record.errors.full_messages
+      render_wizard
+    end
   end
 end
